372dadf6e6d214eaca0c9d482c8e93cbea352aaf
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-09-25 20:42:23 -0300

    feat: html file on express server

--- 2-express-101/express-server.js
+++ 2-express-101/express-server.js
@@ -1,11 +1,17 @@
+const path = require("path");
 const express = require("express");
-
 const app = express();
+const PORT = 3000;
 
-app.all("*", (req, res) => {
-  res.send(`<h1>Express Home Page</h1>`);
+app.use(express.static("public"));
+
+app.all("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/home.html"));
 });
 
-const PORT = 3000;
+app.all("*", (req, res) => {
+  res.send("<h1>Page not found</h1>");
+});
+
 app.listen(PORT);
 console.log(`The server is listening on port ${PORT}.`);
