008f96e060a0829f9541fa72607ea4380ad27ef3
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-09-25 17:08:13 -0300

    feat: add files

--- express-101/node-server.js
+++ express-101/node-server.js
@@ -1,15 +1,43 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  console.log(`--> A request was made to "${req.url}"`);
+
   if (req.url === "/") {
+    const homePage = fs.readFileSync("home.html");
+
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home Page</h1>");
+    res.write(homePage);
     res.end();
+
     return;
   }
 
+  if (req.url === "/node-logo.png") {
+    const nodeLogoImg = fs.readFileSync("node-logo.png");
+
+    res.writeHead(200, { "content-type": "image/png" });
+    res.write(nodeLogoImg);
+    res.end();
+
+    return;
+  }
+
+  if (req.url === "/style.css") {
+    const stylesheet = fs.readFileSync("style.css");
+
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(stylesheet);
+    res.end();
+
+    return;
+  }
+
+  const errorPage = fs.readFileSync("error.html");
+
   res.writeHead(404, { "content-type": "text/html" });
-  res.write("<p>Not found!</p>");
+  res.write(errorPage);
   res.end();
 });
 
