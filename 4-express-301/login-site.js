24ec978d9834da0f9eb527970f605e50089201b0
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-09-26 19:01:02 -0300

    feat: login routes

--- 4-express-301/login-site.js
+++ 4-express-301/login-site.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -8,12 +9,39 @@ app.use(helmet());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res, next) => {
-  res.send("Hello World");
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+app.post("/process_login", (req, res, next) => {
+  const password = req.body.password;
+  const username = req.body.username;
+
+  if (password === "x") {
+    res.cookie("username", username);
+    res.redirect("/welcome");
+    return;
+  }
+
+  res.redirect("/login?msg=fail");
+});
+
+app.get("/welcome", (req, res, next) => {
+  res.render("welcome", { username: req.cookies.username });
+});
+
+app.get("/logout", (req, res, next) => {
+  res.clearCookie("username");
+  res.redirect("/login");
 });
 
 app.listen(3000);
