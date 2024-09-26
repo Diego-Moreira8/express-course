9eee10edba8e90d4113e42a1204abb0912776be7
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-09-26 17:10:14 -0300

    feat: pass data to ejs

--- 3-express-201/ejs-practice.js
+++ 3-express-201/ejs-practice.js
@@ -2,18 +2,28 @@ const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 
+function validateUser(req, res, next) {
+  // ...validation logic
+  res.locals.validated = true;
+  next();
+}
+
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validateUser);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    msg: "Success",
+    html: "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Node.js_logo_2015.svg/2560px-Node.js_logo_2015.svg.png' />",
+  });
 });
 
 app.listen(3000);
