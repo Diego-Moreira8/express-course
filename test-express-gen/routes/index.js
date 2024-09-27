022ed70fcc6cf668af76c68cd0928bd886d2b07e
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-09-27 20:57:02 -0300

    feat: set http header

--- test-express-gen/routes/index.js
+++ test-express-gen/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const date = new Date(1997, 7, 2);
+
+  res.set("Date", date);
+  res.set("Content-type", "text/plain");
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
