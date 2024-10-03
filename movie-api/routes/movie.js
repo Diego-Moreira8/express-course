20c970891c9151ce3d9667887c0ab518addc4c9e
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-10-03 20:34:43 -0300

    feat: api post rating

--- movie-api/routes/movie.js
+++ movie-api/routes/movie.js
@@ -2,6 +2,15 @@ var express = require("express");
 var router = express.Router();
 const movieDetails = require("../data/movies");
 
+function requireJSON(req, res, next) {
+  if (!req.is("application/json")) {
+    res.json({ msg: "Content type must be application/json" });
+    return;
+  }
+
+  next();
+}
+
 router.param("movieId", (req, res, next) => {
   console.log("Someone hit a route that uses :movieId");
   next();
@@ -35,6 +44,19 @@ router.get("/:movieId", (req, res, next) => {
   }
 
   res.json({ ...movie, production_companies: [] });
+});
+
+router.post("/:movieId/rating", requireJSON, (req, res, next) => {
+  const movieId = req.params.movieId;
+  const userRating = parseFloat(req.body.value);
+
+  if (userRating < 0.5 || userRating > 10) {
+    console.log(userRating);
+    res.json({ msg: "Rating must be a value between 0.5 and 10" });
+    return;
+  }
+
+  res.json({ msg: "Thanks!!" });
 });
 
 module.exports = router;
